2160515be8f9deae65210c603228ff6793e0fffe
coding.tarun <coding.tarun@gmail.com>
2024-06-23 10:09:34 +0530

    PHP Basics

--- index.php
+++ index.php
@@ -5,12 +5,22 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>PHP Playground</title>
+    <!-- Bootstrap 5.3.3 Stylesheet -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <!-- Custom Stylesheet -->
+    <link rel="stylesheet" href="assets/style/style.css">
+    <!-- jQuery 3.7.1 -->
+    <script src="assets/script/jQuery-3.7.1.min.js"></script>
+    <!-- Custom Script -->
+    <script src="assets/script/script.js"></script>
 </head>
 
 <body>
-    <h1>Hello, world!</h1>
+    <h1>PHP Playground</h1>
+    <a href="php_basics/index.php">PHP Basics</a>
+    <!-- Bootstrap 5.3.3 Script -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
+
 
 </html>
---
+++ php_basics/index.php
@@ -0,0 +1,39 @@
+<?php
+/*
+|--------------------------------------------------------------------------
+| PHP Operators 
+|--------------------------------------------------------------------------
+|
+| Symbols that tells the PHP processor to perform specific task on provided
+| variables or data.
+| 
+| There is 10 different types of Operator supported by PHP : 
+| 1. Arithmetic Operators
+| 2. Assignment Operators
+| 3. Comparision Operatos
+| 4. Increment/Decrement Operators
+| 5. Logical Operators
+| 6. String Operators
+| 7. Array Operators
+| 8. Bitwise Operators
+| 9. Null Coalescing Operator
+|
+|
+*/
+
+
+/*
+|--------------------------------------------------------------------------
+| Arithmetic Operators
+|--------------------------------------------------------------------------
+|
+| The arithmetic Operators are used to perform basic mathematical operations.
+| 1. Addition (+) : Adds two operands.
+| 2. Subtraction (-) : Subtract the second operand from first.
+| 3. Multiplication (*) : Multiplies two operands
+| 4. Division (/) : Divides  the first operand by the second.
+| 5. Modulus (%) : Returns the remainder of the division.
+| 6. Exponentiation (**) : Raises the first operand to the power of second.
+| 
+|
+*/
